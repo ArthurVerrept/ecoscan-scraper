501f2e7440e1660190898c9ec47b79d126d59026
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2021-12-21 14:31:02 +0000

    make it google the site for better accuracy

--- src/scrapers.ts
+++ src/scrapers.ts
@@ -51,62 +51,42 @@ async function scrapeProduct (barcode: string, trustedSites: string[], browser: 
             img
         }
     } else {
-        // get all divs with links to websites from page 1
-        const divs = await page.$x('//*[@id="rso"]/div')
-
         // for each div we are going to try match to one of our trusted sites
         // and click on it to scrape from there
-        for (let i = 0; i < divs.length; i++) {
+        const sitePage = await browser.newPage()
+        for (let x = 0; x < trustedSites.length; x++) {
+            await sitePage.goto(`https://www.google.com/search?q=${barcode}+${trustedSites[x]}`)
 
-            // get the url of shop using cite html tag it's linking to out the div
-            const citeStr = await divs[i].$('cite')
+            const divs = await sitePage.$x('//*[@id="rso"]/div')
+            console.log(divs.length)
+            for (let i = 0; i < divs.length; i++) {
+                let citeStr = await divs[i].$('cite')
+                if (citeStr) {
+                    let re = new RegExp('.+?(?= )');
+                    let siteUrl = re.exec(await citeStr.evaluate(el => el.textContent))[0]
+                    // console.log(trustedSites[x], siteUrl)
+                    if(siteUrl.includes(trustedSites[x])) {
+                        const siteLink = await divs[i].$('a')
+                        const newSiteURL = await siteLink.evaluate(el => el.getAttribute('href'))
 
-            // if it found a url
-            if(citeStr){
-                // remove extra text (not necessary)
-                let re = new RegExp('.+?(?= )');
-                const siteUrl = re.exec(await citeStr.evaluate(el => el.textContent))[0]
-
-                // for this div, check if it matches on of our trusted sites
-                // this returns a trusted site from our sites to be used later
-                const trustedSite = trustedSites.find(url => siteUrl.includes(url))
-                console.log('found ' + siteUrl)
-                // if one matches go scrape data from there
-                if (trustedSite) {
-                    // TODO: add scraping of data
-                    console.log('matched to: ' + trustedSite)
-                    // get link element
-                    const siteLink = await divs[i].$('a')
-                    // pull out URL
-                    const newSiteURL = await siteLink.evaluate(el => el.getAttribute('href'))
-                    
-                    // create new page
-                    const sitePage = await browser.newPage()
-
-                    // user agent will often state that we are using a headless browser which 
-                    // by default will return a permission denied on a lot of Cloudflare
-                    // services.
-                    // https://stackoverflow.com/questions/68696073/puppeteer-cloudflare-websites-return-403-forbidden
-                    // https://jsoverson.medium.com/how-to-bypass-access-denied-pages-with-headless-chrome-87ddd5f3413c
-                    await sitePage.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36')
-
-                    // go to trusted site page
-                    await sitePage.goto(newSiteURL)
-
-                    // create new instance of scraper of trusted site passed in
-                    let siteScraper = new scraperMap[trustedSite]
-                    const data = await siteScraper.run(sitePage)
-
-                    // close current page before potentially opening another
-                    await sitePage.close()
-
-                    // if data is found it exits for loop aka stops looking at other pages
-                    return data
+                        // user agent will often state that we are using a headless browser which 
+                        // by default will return a permission denied on a lot of Cloudflare
+                        // services.
+                        // https://stackoverflow.com/questions/68696073/puppeteer-cloudflare-websites-return-403-forbidden
+                        // https://jsoverson.medium.com/how-to-bypass-access-denied-pages-with-headless-chrome-87ddd5f3413c
+                        await sitePage.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36')
+                        await sitePage.goto(newSiteURL)
+                        let siteScraper = new scraperMap[trustedSites[x]]
+                        const data = await siteScraper.run(sitePage)
+                        return data
+                    }
                 }
             }
-        }
-        await page.close()
 
+        }
+        await sitePage.close()
+        await page.close()
+        
         console.log('no matches')
         // if we make it here then no trusted sites were found and no data can be returned
         // TODO: return something appropriate
@@ -121,30 +101,10 @@ export default async function getProduct(barcode: string) {
     const product = await scrapeProduct(barcode, trustedSites, browser) 
     
     browser.close()
-
+    
     return product
-
-    // browser = await puppeteer.launch()
-
-    // product = await scrapeProduct('9780141029542', trustedSites, browser)
-    
-    // console.log(product)
-
-    // browser.close()
-
-
-    // browser = await puppeteer.launch()
-
-    // product = await scrapeProduct('4009900482776', trustedSites, browser)
-    
-    // console.log(product)
-
-    // browser.close()
 }
 
 // sainsburys got removed cause the site is shite
 // these are in order of what I have found to be the most consistently correct
-const trustedSites: string[] = ['openfoodfacts', 'appyshop', 'deebee', 'buycott', 'tesco', 'waitrose']
-
-// const [imagesButton] = await page.$x('//*[@id="hdtb-msb"]/div[1]/div/div[4]/a')
-// await imagesButton.click()
+const trustedSites: string[] = ['openfoodfacts', 'appyshop', 'deebee']

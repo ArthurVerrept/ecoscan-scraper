51b31dda8cecea7d08c8611ab5a2a23764183a7d
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2021-12-29 10:36:44 +0000

    add port for cloud

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import getProduct from './scrapers'
 import express from 'express'
 // rest of the code remains same
 const app = express();
-const PORT = 8000;
+const PORT = process.env.port || 8000;
 
 
 app.get('/', async (req, res) => {
